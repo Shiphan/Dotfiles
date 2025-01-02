3d15c09694ab7ec89adebe67e419280324ae51a7
Shiphan <140245703+Shiphan@users.noreply.github.com>
2025-01-03 00:37:14 +0800

    Update scripts

--- dotfiles/eww/statusbar/scripts/get-window-info.sh
+++ dotfiles/eww/statusbar/scripts/get-window-info.sh
@@ -5,29 +5,9 @@ getinfo(){
 }
 
 getinfo
-socat -u "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" - | while read -r line ; do
+
+socat -U - "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line ; do
 	if [[ $line =~ "activewindow>>" ]] || [[ $line =~ "changefloatingmode>>" ]] || [[ $line =~ "fullscreen>>" ]] ; then
 	 	getinfo
 	fi
 done
-
-: '
-class=""
-title=""
-fullscreen=0
-floating=false
-xwayland=false
-
-getinfo(){
-	case $1 in
-		monitoradded*) do_something ;;
-		focusedmon*) do_something_else ;;
-	esac
-
-	echo "{\"class\": \"$class\", \"title\": \"$title\", \"fullscreen\": $fullscreen, \"floating\": $floating, \"xwayland\": $floating}"
-}
-
-socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line ; do
-	getinfo "$line"
-done
-'
