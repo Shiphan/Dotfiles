c373d48e76f9a28b2379414bc23e15cce6aee3db
Shiphan <140245703+Shiphan@users.noreply.github.com>
2025-03-16 18:15:44 +0800

    Open tcp port 8088

--- dotfiles/nixos-system/configuration.nix
+++ dotfiles/nixos-system/configuration.nix
@@ -136,7 +136,7 @@
   # List services that you want to enable:
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 8088 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
