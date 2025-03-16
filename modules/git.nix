840fc7435a4066399bb6bf89eb93a5102291ab2b
Shiphan <140245703+Shiphan@users.noreply.github.com>
2025-03-16 17:57:26 +0800

    Enable git commit signing

--- modules/git.nix
+++ modules/git.nix
@@ -7,7 +7,12 @@
       lfs.enable = true;
       userEmail = "140245703+Shiphan@users.noreply.github.com";
       userName = "Shiphan";
-      # signing = { };
+      signing = {
+        format = "ssh";
+        signByDefault = true;
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINcPTuR3Pb/F7ZarEjHaeSi83Uyzqc/9sD10jvTZKaYB 140245703+Shiphan@users.noreply.github.com";
+        # key = builtins.readFile (config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.ssh/id_ed25519");
+      };
       extraConfig = {
         init.defaultBranch = "main";
       };
