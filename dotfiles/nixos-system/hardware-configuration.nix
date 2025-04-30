0f3f37c143e0eb199e42608230a1d40d101c6511
Shiphan <140245703+Shiphan@users.noreply.github.com>
2025-04-30 10:52:02 +0800

    Reinstall nixos

--- dotfiles/nixos-system/hardware-configuration.nix
+++ dotfiles/nixos-system/hardware-configuration.nix
@@ -19,12 +19,14 @@
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/4498-4227";
+    { device = "/dev/disk/by-uuid/86A9-6414";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/f11a44c0-252a-483c-b3ba-94ad9ed09048"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
--- dotfiles/nixos-system/modules/suspend.nix
+++ dotfiles/nixos-system/modules/suspend.nix
@@ -1,20 +1,6 @@
 { ... }:
 
 {
-  swapDevices = [
-    {
-      device = "/swapfile";
-      size = 28*1024;
-    }
-  ];
-
-  boot = {
-    resumeDevice = "/dev/nvme0n1p6";
-    kernelParams = [
-      "resume_offset=23891968"
-    ];
-  };
-
   systemd.sleep.extraConfig = ''
     AllowSuspend=yes
     AllowHibernation=yes
