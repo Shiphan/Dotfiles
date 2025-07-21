032b8929b48ddafb4bdc042646de23cb9c697b5d
Shiphan <140245703+Shiphan@users.noreply.github.com>
2025-07-21 15:46:40 +0800

    Hide grub menu on boot and add entry of uefi firmware

--- dotfiles/nixos-system/modules/boot.nix
+++ dotfiles/nixos-system/modules/boot.nix
@@ -2,6 +2,8 @@
 
 {
   boot.loader = {
+    # NOTE: on my framework 13, for showing grub menu, instead of holding shift key, you should spam esc
+    timeout = 0;
     efi = {
       canTouchEfiVariables = true;
       efiSysMountPoint = "/boot";
@@ -13,6 +15,14 @@
       useOSProber = true;
       default = "saved";
       # theme = "${pkgs.kdePackages.breeze-grub}/grub/themes/breeze";
+      timeoutStyle = "countdown";
+      extraEntries = ''
+        if [ ''${grub_platform} == "efi" ]; then
+          menuentry "UEFI Firmware Settings" --id "uefi-firmware" {
+            fwsetup
+          }
+        fi
+      '';
     };
   };
 }
