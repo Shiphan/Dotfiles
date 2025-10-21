9b5e3b016f6d6019b4ce35e30665c95036ebf213
Shiphan <140245703+Shiphan@users.noreply.github.com>
2025-10-21 18:17:53 +0800

    Flake update

--- modules/git.nix
+++ modules/git.nix
@@ -5,17 +5,21 @@
     git = {
       enable = true;
       lfs.enable = true;
-      userEmail = "140245703+Shiphan@users.noreply.github.com";
-      userName = "Shiphan";
       signing = {
         format = "ssh";
         signByDefault = true;
         key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINcPTuR3Pb/F7ZarEjHaeSi83Uyzqc/9sD10jvTZKaYB 140245703+Shiphan@users.noreply.github.com";
         # key = builtins.readFile (config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.ssh/id_ed25519");
       };
-      extraConfig = {
-        init.defaultBranch = "main";
-        merge.tool = "nvimdiff";
+      settings = {
+        user = {
+          email = "140245703+Shiphan@users.noreply.github.com";
+          name = "Shiphan";
+        };
+        extraConfig = {
+          init.defaultBranch = "main";
+          merge.tool = "nvimdiff";
+        };
       };
     };
     gh = {
