23d30d809bf1904106cbc1f5139bf1ea9c034a15
Shiphan <140245703+Shiphan@users.noreply.github.com>
2025-12-16 01:27:03 +0800

    Use my hyprlock patch (allow mixed percentage/pixel)

--- modules/desktop-environment.nix
+++ modules/desktop-environment.nix
@@ -28,7 +28,15 @@
 
     eww
     quickshell
-    hyprlock
+    (hyprlock.overrideAttrs (previousAttrs: {
+      patches = (previousAttrs.patches or []) ++ [
+        # Add support for mixed percentage/pixel in one axis of a layout value
+        (fetchpatch2 {
+          url = "https://github.com/Shiphan/hyprlock/commit/d9e53881e729a6be304ab5b30018bd5124306e70.patch";
+          hash = "sha256-UKal1aEnorjzCTroox0DCb+ZvcWHsFVy/vkZ5WDMMqQ=";
+        })
+      ];
+    }))
     hyprpaper
     hyprsunset
     hyprpicker
